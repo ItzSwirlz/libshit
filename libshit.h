f9fa1901a3b03e0537f59eba935d4adf95ad870e
Joshua Peisach <itzswirlz2020@outlook.com>
2022-10-06 22:04:42 -0400

    help me

---
+++ libshit.h
@@ -0,0 +1,64 @@
+// idk
+// pulled from this list: https://en.wikipedia.org/wiki/C_data_types
+
+// chars
+// more like char-coal
+typedef char shit_char;
+typedef signed char shit_signed_char;
+typedef unsigned char shit_unsigned_char;
+
+// shorts integers
+// the datatype refers to them as "D", so i'll call them "dintegers"
+// shorts, or as i like to call them, weenies
+typedef short shit_short;  // this actually is probably the best type of them all
+typedef short int shit_short_int;
+typedef signed short shit_signed_short;
+typedef signed short int shit_signed_short_int;
+
+// unsigned shorts
+// marcus & martinus fans probably crying rn
+typedef unsigned short shit_unsigned_short;
+typedef unsigned short int shit_unsigned_short_int;
+
+// dintegers, for realsies
+typedef int shit_int;
+typedef signed shit_signed;
+typedef signed int shit_signed_int;
+
+// dintegers but unsigned
+typedef unsigned shit_unsigned;
+typedef unsigned int shit_unsigned_int;
+
+// long, and big bois
+typedef long shit_long;
+typedef long int shit_long_int;
+typedef signed long shit_signed_long;
+typedef signed long int shit_signed_long_int;
+
+// unsigned big bois
+typedef unsigned long shit_unsigned_long;
+typedef unsigned long int shit_unsigned_long_int;
+
+// long long big bois
+typedef long long shit_long_long;
+typedef long long int shit_long_long_int;
+typedef signed long long shit_signed_long_long;
+typedef signed long long int shit_signed_long_long_int;
+
+// unsigned big big long bois
+typedef unsigned long long shit_unsigned_long_long;
+typedef unsigned long long int shit_unsigned_long_long_int;
+
+// floats
+// fat floats, to be exact
+typedef float shit_float;
+
+// double double this this double double that that double this double that double double this that
+typedef double shit_double;
+
+// double double this this double double that that double this double that double double this that but longer
+typedef long double shit_long_double;
+
+// eh minus well
+typedef void shit_void;
+// i would do classes and stuff but i don't want to break arguments
